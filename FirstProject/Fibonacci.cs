990f01d62f3c0f30b0c8e6a80657d952e02b952d
boluwatife.olubajo <boluwatife.olubajo@influentialsoftware.com>
2019-08-01 14:33:15 +0100

    Add project files.

---
+++ FirstProject/Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Fibonacci
+    {
+        private int startNumber;
+        private int nextNumber;
+
+        public void PrintFibonacciNumbers()
+        {
+            startNumber = 0;
+            nextNumber = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+   
+                Console.Write(startNumber + ",");
+
+                int sum = startNumber + nextNumber;
+
+                startNumber = nextNumber;
+                nextNumber = sum;
+
+            }
+        }
+    }
+}
---
+++ FirstProject/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program 
+    {
+       
+        static void Main(string[] args)
+        {
+            Fibonacci fibonacci = new Fibonacci();
+            fibonacci.PrintFibonacciNumbers();
+            Console.ReadKey(true);
+            
+           
+        } 
+
+     
+    }
+}
